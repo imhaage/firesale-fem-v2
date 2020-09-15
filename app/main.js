1aa0b948d1fb191cc26c809dcd6370fd206cc6a0
imhaage <mahaage@gmail.com>
2020-09-15 15:29:00 +0200

    First window

--- app/main.js
+++ app/main.js
@@ -1,1 +1,18 @@
-console.log('This is an Electron application!');
+// electron has access to Node API + browser API
+const { app, BrowserWindow } = require("electron");
+
+// global to avoid garbage collection
+let mainWindow = null;
+
+app.on("ready", () => {
+  // create window and hide it
+  mainWindow = new BrowserWindow({ show: false });
+
+  mainWindow.loadFile(`${__dirname}/index.html`);
+
+  // wait until file is loaded
+  mainWindow.once("ready-to-show", () => {
+    // show window
+    mainWindow.show();
+  });
+});
